450bc5fc2f536ba93e376c10fdbd31dbf851a8b5
Harold Gao <harold.gao205@gmail.com>
2025-11-16 17:03:10 +1100

    makefile and init directories

---
+++ Makefile
@@ -0,0 +1,10 @@
+# Set of C compiler to gcc
+CC=gcc
+CFLAGS= -Wall -Wconversion -fsanitize=address
+
+all: src/main.c
+	$(CC) $(CFLAGS) $^ -o gradethink
+
+.PHONY: clean 
+clean: 
+	rm src/*.o gradethink >/dev/null 2>/dev/null || true
--- src/main.c
+++ src/main.c
@@ -2,15 +2,47 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <dirent.h>
+#include <errno.h>
 
-int init_directories(){
+#define MAXDIRLEN 256
+
+/**
+ * Initialises the directories,
+ * @return a pointer to a string representing the path of the newly created directory, or NULL on fail.
+ */
+char *init_directories(){
     char *home = getenv("HOME");
-    char path[128];
-    strncpy(path, home, 128);
-    strlcat(path, "/.config", 128);
-    if(mkdir(path, 0700) != -1){
-        printf("%s\n", "Creating .config subdirectory.");
+    char *path = malloc(MAXDIRLEN);
+    strncpy(path, home, MAXDIRLEN);
+    strlcat(path, "/.config", MAXDIRLEN);
+
+    DIR *config = opendir(path);
+    if(config == NULL){
+        if(errno == ENOENT){
+            printf("%s\n", "Error: Directory .config does not exist in your home directory. Please create this directory first.");
+        }else{
+            printf("%s\n", "Error: Failed to open .config directory, make sure your permission bits are properly set.");
+        }
+        free(path);
+        return NULL;
     }
+    closedir(config);
+
+    strlcat(path, "/gradethink", MAXDIRLEN);
+
+    int mkdirres = mkdir(path, 0700);
+    if(mkdirres == -1){
+        if(errno != EEXIST){
+            printf("%s\n", "Failed to initialise subdirectory for gradethink in ~/.config. Make sure the permission bits are properly set.");
+            free(path);
+            return NULL;
+        }
+    }else{
+        printf("%s\n", "Creating subdirectory for gradethink in ~/.config.");
+    }
+
+    return path;
 }
 
 
@@ -21,6 +53,8 @@ int main(int argc, char **argv) {
         printf("%s\n", "Error: Too few arguments.");
         return 1;
     }
+
+    char *path = init_directories();
 
     char *command = argv[1];
 
@@ -39,6 +73,7 @@ int main(int argc, char **argv) {
 
     }
 
+    free(path);
     return 0;
 
 }
