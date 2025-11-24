bce5081edbdd63dd311527eea4851460e13434f6
Harold Gao <u7869833@anu.edu.au>
2025-11-24 15:54:32 +1100

    implemented file reading

--- src/main.c
+++ src/main.c
@@ -2,8 +2,39 @@
 #include <stdio.h>
 
 /**
+* Reads the content of the file line by line, each line. Each 
+* line will be parsed separately by the parser.
+*/ 
+void parse_file(char *file_path){
+    
+    FILE *file = fopen(file_path, "r");
+    if(file == NULL){
+        printf("Error: Cannot read file.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    while((read = getline(&line, &len, file)) != -1){
+        printf("%s", line);
+    }
+    free(line);
+    return;
+}
+
+/**
 * Main routine.
 */
-int main(){
+int main(int argc, char *argv[]){
+    
+    if(argc < 2){
+        printf("Error: Too few arguments.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    char *file_path = argv[1];
+    parse_file(file_path);
+    
     return 0;
 }
